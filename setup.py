9cabfaaa4a00e13452541f87cebfb550713c8d11
Shivam Thassu <shivamtv2@gmail.com>
2019-01-04 20:07:19 +0530

    Update Setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='AcronymFinder',
+    version='1.0',
+    packages=['acronymfinder'],
+    license='MIT License',
+    url = "https://github.com/darshan22/DATS6450-Final-Project",
+    description = open('README.txt').read(),
+)
