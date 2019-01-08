923b4496460aa5224be973615eb0e97dd7f9c6a1
Shivam Thassu <shivamtv2@gmail.com>
2019-01-08 23:39:00 +0530

    Comments update

--- acronymfinder/acr_extractor.py
+++ acronymfinder/acr_extractor.py
@@ -27,11 +27,12 @@ class AcronymFinder:
         files_list = listdir(self.path)
         dictionary_acronyms = {}
         acronym_doc_num = {}
+        
+        # Read all the files in the directory and store data in a list one by one by file.
         for num, val in enumerate(files_list):
             input_text = open(self.path + val, encoding = "utf-8").read()
     
-   
-
+        # Regex Pattern
             acronym = re.findall(r'\([A-Z][A-Za-z\.][A-Z]+\)', input_text)
             acronym_list = []
             for a in acronym:
@@ -39,9 +40,8 @@ class AcronymFinder:
                 sample.remove('(')
                 sample.remove(')')
                 acronym_list.append("".join(sample))
-
-        
-        
+    
+        # Read the text data and tokenize it first by sentence then by word in that sentence.
             sentence_tokens = sentence_tokenizer.tokenize(input_text)
             sentences = [treebank_tokenizer.tokenize(sentence) for sentence in sentence_tokens]
             for sentence in sentences:
@@ -53,12 +53,16 @@ class AcronymFinder:
                             if (acronym_sent not in list(dictionary_acronyms.keys())):
                                 dictionary_acronyms[acronym_sent] = get_acronym.strip_acronym(acronym_sent, sentence1)
                                 acronym_doc_num[acronym_sent] = num+1
+                                
+        # Define and Map the dictionary                        
         final_dict = {"Acronym" : [], "Full-form" : [], "Document-number" : []}
         acr_list = [key for key, value in dictionary_acronyms.items()]
         for acronym in acr_list:
             final_dict["Acronym"].append(acronym)
             final_dict["Full-form"].append(dictionary_acronyms[acronym])
             final_dict["Document-number"].append(acronym_doc_num[acronym])
+            
+        # Save as dataframe and output a csv    
         df = pd.DataFrame(final_dict)
         df.to_csv('Final_dictionary.csv')
-
+        
